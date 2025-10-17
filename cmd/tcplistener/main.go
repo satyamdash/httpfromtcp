43779e6478b7164fb216418c9e017b4dbf092d10
satyamdash <satyam.bn6@gmail.com>
2025-10-17 21:10:08 +0530

    Requestline with chunk handling

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -7,42 +7,44 @@ import (
 	"github.com/satyamdash/httpfromtcp/internal/request"
 )
 
-// func getLinesChannel(f io.ReadCloser) <-chan string {
-// 	str_chan := make(chan string)
+/*
+	func getLinesChannel(f io.ReadCloser) <-chan string {
+		str_chan := make(chan string)
 
-// 	go func() {
-// 		var curline string
-// 		buf := make([]byte, 8)
-// 		defer f.Close()
-// 		for {
-// 			n, err := f.Read(buf)
-// 			if n > 0 {
-// 				chunks := string(buf[:n])
-// 				parts := strings.Split(chunks, "\n")
-// 				for i := 0; i < len(parts)-1; i++ {
-// 					str_chan <- curline + parts[i]
-// 					curline = ""
-// 				}
+		go func() {
+			var curline string
+			buf := make([]byte, 8)
+			defer f.Close()
+			for {
+				n, err := f.Read(buf)
+				if n > 0 {
+					chunks := string(buf[:n])
+					parts := strings.Split(chunks, "\n")
+					for i := 0; i < len(parts)-1; i++ {
+						str_chan <- curline + parts[i]
+						curline = ""
+					}
 
-// 				curline += parts[len(parts)-1]
-// 			}
-// 			if err == io.EOF {
-// 				// file ended; print whatever is left
-// 				if len(curline) > 0 {
-// 					str_chan <- curline
-// 				}
-// 				close(str_chan)
-// 				break
-// 			}
-// 			if err != nil {
-// 				break
-// 			}
+					curline += parts[len(parts)-1]
+				}
+				if err == io.EOF {
+					// file ended; print whatever is left
+					if len(curline) > 0 {
+						str_chan <- curline
+					}
+					close(str_chan)
+					break
+				}
+				if err != nil {
+					break
+				}
 
-// 		}
-// 	}()
+			}
+		}()
 
-// 	return str_chan
-// }
+		return str_chan
+	}
+*/
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
